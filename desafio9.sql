5c0a32f1c941fc9c0db588a9b1fafc732d7a9f7a
Felipe Gama Danta <epilefgama2@gmail.com>
2022-06-03 17:58:37 -0300

    fix: Requisito 9, ajusta a query para usar o nome Bill como referencia

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,5 @@
 SELECT 
 	COUNT(*) AS quantidade_musicas_no_historico
-FROM reprodution_history
-WHERE user_id = 3
+FROM reprodution_history as r
+inner join user AS u on u.id = r.user_id
+WHERE u.name = 'Bill'
